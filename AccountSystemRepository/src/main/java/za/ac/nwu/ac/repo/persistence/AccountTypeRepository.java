46f39f7d27133f9a29714a2b8439d0256b0dcefe
MichellevanRensburg <vanrensburg4m@gmail.com>
2021-09-29 20:35:31 +0200

    Addedmore code to custom query to database
    Added code to the repository and translator

--- AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
+++ AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
@@ -1,9 +1,37 @@
 package za.ac.nwu.ac.repo.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import za.ac.nwu.ac.domain.dto.AccountTypeDto;
 import za.ac.nwu.ac.domain.persistense.AccountType;
 
 @Repository
 public interface AccountTypeRepository extends JpaRepository<AccountType, Long> {
+
+    @Query(value = "SELECT" +
+            "         ACCOUNT_TYPE_ID," +
+            "         ACCOUNT_TYPE_NAME," +
+            "         CREATION_DATE," +
+            "         MNEMONIC," +
+            "     FROM " +
+            "         VITRSA_SANDBOX.DEMO_ACCOUNT_TYPE" +
+            "     WHERE MNEMONIC = :mnemonic ", nativeQuery = true)
+    AccountType getAccountTypeByMnemonicNativeQuery(String mnemonic);
+
+    @Query(value = "SELECT" +
+            "         at" +
+            "     FROM " +
+            "         AccountType at" +
+            "     WHERE at.mnemonic = :mnemonic ", nativeQuery = true)
+    AccountType getAccountTypeByMnemonic(String mnemonic);
+
+    @Query(value = "SELECT new za.ac.nwu.domain.dto.AccountTypeDto(" +
+            "         at.mnemonic," +
+            "         at.accountTypeName," +
+            "         at.creationDate," +
+            "     FROM " +
+            "         AccountType at" +
+            "     WHERE at.mnemonic = :mnemonic ", nativeQuery = true)
+    AccountTypeDto getAccountTypeDtoByMnemonic(String mnemonic);
 }
