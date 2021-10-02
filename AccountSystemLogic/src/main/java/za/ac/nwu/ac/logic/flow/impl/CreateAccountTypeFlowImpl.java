96f7dcc12bd51e085b2b54c8dbc5350b46be4e7d
MichellevanRensburg <vanrensburg4m@gmail.com>
2021-10-02 14:21:38 +0200

    Mockito unit testing
    Created a testing Mockito class for CreateAccountTypeFlowImplTest on Account System Translator.
    2021/10/02

--- AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/CreateAccountTypeFlowImpl.java
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/CreateAccountTypeFlowImpl.java
@@ -3,6 +3,7 @@ package za.ac.nwu.ac.logic.flow.impl;
 
 import org.springframework.stereotype.Component;
 import za.ac.nwu.ac.domain.dto.AccountTypeDto;
+import za.ac.nwu.ac.logic.flow.CreateAccountTypeFlow;
 import za.ac.nwu.ac.translator.AccountTypeTranslator;
 
 import javax.transaction.Transactional;
@@ -10,11 +11,11 @@ import java.time.LocalDate;
 
 @Transactional
 @Component("createAccountTypeFlowName")
-public class CreateAccountTypeFlowImpl implements CreateAccountTypeFlowImpl {
+public class CreateAccountTypeFlowImpl implements CreateAccountTypeFlow {
 
-    private final AccountTypeTranlator accountTypeTranlator;
+    private final AccountTypeTranslator accountTypeTranslator;
 
-    public za.ac.nwu.ac.logic.flow.CreateAccountTypeFlowImpl(AccountTypeTranlator accountTypeTranlator){
+    public CreateAccountTypeFlowImpl(AccountTypeTranslator accountTypeTranslator){
         this.accountTypeTranslator = accountTypeTranslator;
     }
 
